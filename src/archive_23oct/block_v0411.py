8b0ccc81048ead8ae618faf7efb0da449c587548
charudatta <152109007c@gmail.com>
2024-11-04 13:45:07 +0530

    major refactoring v 5.0

--- src/archive_23oct/block_v0411.py
+++ src/archive_23oct/block_v0411.py
@@ -4,41 +4,33 @@ import time
 import jwt
 import logging
 from cryptography.hazmat.primitives import serialization
-from wallet import Wallet
-
-# Setup logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class Block:
-    def __init__(self):
+    def __init__(self, index, transactions, previous_token, wallet):
+        self.private_key = wallet.get_private_key_obj()
+        self.public_key = wallet.get_public_key_obj()
         self.difficulty = 1
+        self.payload = self.create_payload(index, 0, transactions, previous_token, self.public_key)
+        self.token = None
 
-    def __str__(self):
-        return json.dumps(self.payload, indent=4) if hasattr(self, 'payload') else 'Un-minted Block'
+    def mint_block(self):
+        self.payload = self.proof_of_work(self.payload)
+        self.token = self.create_jwt(self.payload, self.private_key)
 
-    def mint_block(self, index, transactions, previous_token, wallet):
-        verified_token = self.verify_jwt(previous_token, wallet.get_public_key_obj())
+    def create_payload(self, index, nonce, transactions, previous_token, public_key):
+        verified_token = self.verify_jwt(previous_token, public_key)
         if not verified_token:
             return None
         del verified_token["hash"]
         previous_hash = self.compute_hash(verified_token)
-        payload = self.create_payload(index, 0, transactions, previous_hash)
-        payload = self.proof_of_work(payload)
-        token = self.create_jwt(payload, wallet.get_private_key_obj())
-        self.payload = payload
-        self.token = token
-        return token
-
-    @staticmethod
-    def create_payload(index, nonce, transactions, previous_hash):
         payload = {
             "index": index,
             "nonce": nonce,
             "transactions": transactions,
             "timestamp": time.time(),
-            "previous_hash": previous_hash,
+            "previous_hash": previous_hash
         }
-        payload["hash"] = Block.compute_hash(payload)
+        payload["hash"] = self.compute_hash(payload)
         return payload
 
     @staticmethod
@@ -54,19 +46,17 @@ class Block:
                 return payload
             payload["nonce"] += 1
 
-    @staticmethod
-    def create_jwt(payload, private_key):
+    def create_jwt(self, payload, private_key):
         return jwt.encode(payload, private_key, algorithm="RS256")
 
-    @staticmethod
-    def verify_jwt(token, public_key):
+    def verify_jwt(self, token, public_key):
         try:
             return jwt.decode(token, public_key, algorithms=["RS256"])
         except (jwt.ExpiredSignatureError, jwt.InvalidTokenError):
             return None
 
     @staticmethod
-    def create_genesis_token(private_key_bytes):
+    def create_genesis_token(private_key):
         payload = {
             "index": 0,
             "nonce": 0,
@@ -75,8 +65,19 @@ class Block:
             "previous_hash": "0"
         }
         payload["hash"] = Block.compute_hash(payload)
-        private_key_obj = serialization.load_pem_private_key(private_key_bytes, password=None)
-        token = jwt.encode(payload, private_key_obj, algorithm="RS256")
+        token = jwt.encode(payload, private_key, algorithm="RS256")
         with open("Genesis-Token.txt", "w") as f:
             f.write(token)
         return token
+
+    @staticmethod
+    def token_to_block(token, wallet):
+        public_key = wallet.get_public_key_obj()
+        decoded = jwt.decode(token, public_key, algorithms=["RS256"])
+        block = Block(decoded['index'], decoded['transactions'], decoded['previous_hash'], wallet)
+        block.payload = decoded
+        block.token = token
+        return block
+
+    def block_to_token(self):
+        return self.token
