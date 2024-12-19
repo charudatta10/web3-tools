4885b238d1f3addacb8a3c7801eee37b16b4c611
charudatta <152109007c@gmail.com>
2024-12-19 10:23:50 +0000

    Dec 19, 2024, 3:53 PM

--- src/MISA_VM.py
+++ src/MISA_VM.py
@@ -2,7 +2,7 @@ class CustomVM:
     def __init__(self, memory):
         self.memory = memory  # Initial memory setup
         self.pc = 0  # Program counter
-        self.registers = [10] * 10  # 10 general-purpose registers
+        self.registers = [0] * 10  # 10 general-purpose registers
         self.stack = []
         self.instructions = {
             0: self.add,
@@ -43,71 +43,93 @@ class CustomVM:
             else:
                 raise ValueError(f"Unknown instruction {op_code}")
 
+    def validate_register_index(self, index):
+        if index < 0 or index >= len(self.registers):
+            raise IndexError(f"Register index {index} out of range.")
+
     def add(self):
         a = self.memory[self.pc]
         b = self.memory[self.pc + 1]
+        self.validate_register_index(a)
+        self.validate_register_index(b)
         self.registers[a] += self.registers[b]
         self.pc += 2
 
     def sub(self):
         a = self.memory[self.pc]
         b = self.memory[self.pc + 1]
+        self.validate_register_index(a)
+        self.validate_register_index(b)
         self.registers[a] -= self.registers[b]
         self.pc += 2
 
     def mul(self):
         a = self.memory[self.pc]
         b = self.memory[self.pc + 1]
+        self.validate_register_index(a)
+        self.validate_register_index(b)
         self.registers[a] *= self.registers[b]
         self.pc += 2
 
     def div(self):
-       a = self.memory[self.pc]
-       b = self.memory[self.pc + 1]
-       if self.registers[b] == 0:
-           raise ZeroDivisionError(f"Division by zero error: register[{b}] is zero.")
-       self.registers[a] //= self.registers[b]
-       self.pc += 2
-
+        a = self.memory[self.pc]
+        b = self.memory[self.pc + 1]
+        self.validate_register_index(a)
+        self.validate_register_index(b)
+        if self.registers[b] == 0:
+            raise ZeroDivisionError(f"Division by zero error: register[{b}] is zero.")
+        self.registers[a] //= self.registers[b]
+        self.pc += 2
 
     def and_op(self):
         a = self.memory[self.pc]
         b = self.memory[self.pc + 1]
+        self.validate_register_index(a)
+        self.validate_register_index(b)
         self.registers[a] &= self.registers[b]
         self.pc += 2
 
     def or_op(self):
         a = self.memory[self.pc]
         b = self.memory[self.pc + 1]
+        self.validate_register_index(a)
+        self.validate_register_index(b)
         self.registers[a] |= self.registers[b]
         self.pc += 2
 
     def xor_op(self):
         a = self.memory[self.pc]
         b = self.memory[self.pc + 1]
+        self.validate_register_index(a)
+        self.validate_register_index(b)
         self.registers[a] ^= self.registers[b]
         self.pc += 2
 
     def not_op(self):
         a = self.memory[self.pc]
+        self.validate_register_index(a)
         self.registers[a] = ~self.registers[a]
         self.pc += 1
 
     def load(self):
         a = self.memory[self.pc]
         address = self.memory[self.pc + 1]
+        self.validate_register_index(a)
         self.registers[a] = self.memory[address]
         self.pc += 2
 
     def store(self):
         a = self.memory[self.pc]
         address = self.memory[self.pc + 1]
+        self.validate_register_index(a)
         self.memory[address] = self.registers[a]
         self.pc += 2
 
     def move(self):
         a = self.memory[self.pc]
         b = self.memory[self.pc + 1]
+        self.validate_register_index(a)
+        self.validate_register_index(b)
         self.registers[b] = self.registers[a]
         self.pc += 2
 
@@ -139,17 +161,20 @@ class CustomVM:
 
     def hash_op(self):
         a = self.memory[self.pc]
+        self.validate_register_index(a)
         self.registers[a] = sum(map(ord, str(self.registers[a])))
         self.pc += 1
 
     def sign(self):
         a = self.memory[self.pc]
+        self.validate_register_index(a)
         private_key = "private_key"
         self.registers[a] = hash(self.registers[a] + hash(private_key))
         self.pc += 1
 
     def verify(self):
         a = self.memory[self.pc]
+        self.validate_register_index(a)
         public_key = "public_key"
         self.registers[a] = (self.registers[a] == hash(self.registers[a] + hash(public_key)))
         self.pc += 1
@@ -167,7 +192,7 @@ initial_memory = [
     0, 1, 2,  # ADD R1, R2
     1, 1, 2,  # SUB R1, R2
     2, 1, 2,  # MUL R1, R2
-    3, 1, 2,  # DIV R1, R2
+    3, 1, 2,  # DIV R1, R2 (ensure R2 is not zero)
     11, 4,    # JMP to address 4
     10, 1, 3, # MOVE R1 to R3
 ]
